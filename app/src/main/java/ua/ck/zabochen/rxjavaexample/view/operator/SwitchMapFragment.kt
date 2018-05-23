ca797480033e8616e84a690203c0fb3ea1c90333
zabochen <zabochen@gmail.com>
2018-05-23 11:34:46 +0300

    RxJava - SwitchMap

---
+++ app/src/main/java/ua/ck/zabochen/rxjavaexample/view/operator/SwitchMapFragment.kt
@@ -0,0 +1,85 @@
+package ua.ck.zabochen.rxjavaexample.view.operator
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import io.reactivex.Observable
+import io.reactivex.ObservableSource
+import io.reactivex.Observer
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.functions.Function
+import io.reactivex.schedulers.Schedulers
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+import ua.ck.zabochen.rxjavaexample.R
+import java.util.concurrent.TimeUnit
+
+class SwitchMapFragment : Fragment(), AnkoLogger {
+
+    /*
+    * SwitchMap always return the latest Observable and emits the items from it.
+    */
+
+    private lateinit var mDisposable: Disposable
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_switch_map, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Observable
+        val observable: Observable<Int> = getObservable()
+
+        // Subscription
+        observable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .switchMap(
+                        object : Function<Int, ObservableSource<Int>> {
+                            override fun apply(t: Int): ObservableSource<Int> {
+                                return Observable
+                                        .fromArray(t)
+                                        .delay(2, TimeUnit.SECONDS)
+                            }
+                        }
+                )
+                .subscribe(
+                        object : Observer<Int> {
+                            override fun onSubscribe(d: Disposable) {
+                                mDisposable = d
+                                info { "Subscribe" }
+                            }
+
+                            override fun onNext(t: Int) {
+                                info { "Next => $t" }
+                            }
+
+                            override fun onComplete() {
+                                info { "Complete" }
+                            }
+
+                            override fun onError(e: Throwable) {
+                                info { "Error => ${e.printStackTrace()}" }
+                            }
+                        }
+                )
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+
+        // Unsubscribe
+        mDisposable.dispose()
+        info { "Unsubscribe" }
+    }
+
+    private fun getObservable(): Observable<Int> {
+        return Observable.just(1, 2, 3, 4, 5)
+    }
+
+}
