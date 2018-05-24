5d7d325414bd19ff58a09c5bb3fe91e16c634a44
zabochen <zabochen@gmail.com>
2018-05-24 16:28:30 +0300

    RxJava - Debounce

--- app/src/main/java/ua/ck/zabochen/rxjavaexample/view/main/MainActivity.kt
+++ app/src/main/java/ua/ck/zabochen/rxjavaexample/view/main/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.FrameLayout
 import ua.ck.zabochen.rxjavaexample.R
-import ua.ck.zabochen.rxjavaexample.view.operator.BufferFragment
+import ua.ck.zabochen.rxjavaexample.view.operator.DebounceFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
     private fun setFragment() {
         supportFragmentManager
                 .beginTransaction()
-                .replace(fragmentHolder.id, BufferFragment())
+                .replace(fragmentHolder.id, DebounceFragment())
                 .commit()
     }
 
